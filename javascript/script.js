bb409498ba05ce51fe106faa3fcb12ba273d6197
dougalcaleb <dougalcaleb@gmail.com>
2020-10-07 07:49:59 -0600

    Add 3 placeholder highlight cards

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,17 @@
 var mobileMenu = false;
 
-alert(screen.width);
-
 document.querySelector(".burger").addEventListener("click", function() {
     if (mobileMenu) {
         document.querySelector(".mobile-nav").classList.remove("mobile-nav-active");
+        document.querySelector(".burg0").classList.remove("burg0-active");
         document.querySelector(".burg1").classList.remove("burg1-active");
         document.querySelector(".burg2").classList.remove("burg2-active");
-        document.querySelector(".burg3").classList.remove("burg3-active");
         mobileMenu = false;
     } else {
         document.querySelector(".mobile-nav").classList.add("mobile-nav-active");
+        document.querySelector(".burg0").classList.add("burg0-active");
         document.querySelector(".burg1").classList.add("burg1-active");
         document.querySelector(".burg2").classList.add("burg2-active");
-        document.querySelector(".burg3").classList.add("burg3-active");
         mobileMenu = true;
     }
 });
