7a95706a7f9b569394066ad4b53071dc2e97abdb
Caleb <dougalcaleb@gmail.com>
2020-11-18 17:03:30 -0700

    Fixed a bug causing the page to freeze that didn't show up earlier

--- portfolio/golf scorecard/javascript/load.js
+++ portfolio/golf scorecard/javascript/load.js
@@ -3,8 +3,10 @@ document.body.onload = function() {
         document.querySelector(".load-overlay").style.opacity = "0";
         document.querySelector(".load-overlay").style.transition = "0.2s";
         document.querySelector(".load-overlay").style.visibility = "hidden";
-        if (JSON.parse(localStorage.getItem("settings")).tg_select) {
-            selectCourse(JSON.parse(localStorage.getItem("settings")).tg_select_value);
+        if (localStorage.getItem("settings")) {
+            if (JSON.parse(localStorage.getItem("settings")).tg_select) {
+                selectCourse(JSON.parse(localStorage.getItem("settings")).tg_select_value);
+            }
         }
     }, 500);
 };
