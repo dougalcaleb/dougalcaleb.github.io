68b497f87776da05823657e9855e13feead15f18
dougalcaleb <dougalcaleb@gmail.com>
2021-02-24 12:15:47 -0700

    improvements to docs

--- roundabout/javascript/docs.js
+++ roundabout/javascript/docs.js
@@ -1,189 +1,43 @@
-let settings = [//45
-   {
-      category: "general",
-      name: "autoGenCSS",
-   },
-   {
-      category: "behavior",
-      name: "autoscroll",
-   },
-   {
-      category: "behavior",
-      name: "autoscrollDirection",
-   },
-   {
-      category: "behavior",
-      name: "autoscrollPauseOnHover",
-   },
-   {
-      category: "behavior",
-      name: "autoscrollSpeed",
-   },
-   {
-      category: "behavior",
-      name: "autoscrollStartAfter",
-   },
-   {
-      category: "behavior",
-      name: "autoscrollTimeout",
-   },
-   {
-      category: "pages",
-      name: "backgroundImage",
-   },
-   {
-      category: "pages",
-      name: "css",
-   },
-   {
-      category: "type-specific",
-      name: "direction",
-   },
-   {
-      category: "type-specific",
-      name: "enlargeCenter",
-   },
-   {
-      category: "pages",
-      name: "html",
-   },
-   {
-      category: "general",
-      name: "id",
-   },
-   {
-      category: "behavior",
-      name: "infinite",
-   },
-   {
-      category: "behavior",
-      name: "keys",
-   },
-   {
-      category: "general",
-      name: "mobile",
-   },
-   {
-      category: "general",
-      name: "mobileBreakpoint",
-   },
-   {
-      category: "general",
-      name: "navigation",
-   },
-   {
-      category: "behavior",
-      name: "navigationBehavior",
-   },
-   {
-      category: "type-specific",
-      name: "offsetIn",
-   },
-   {
-      category: "type-specific",
-      name: "offsetOut",
-   },
-   {
-      category: "type-specific",
-      name: "offsetUnits",
-   },
-   {
-      category: "general",
-      name: "pages",
-   },
-   {
-      category: "type-specific",
-      name: "pageSpacing",
-   },
-   {
-      category: "type-specific",
-      name: "pageSpacingUnits",
-   },
-   {
-      category: "type-specific",
-      name: "pagesToShow",
-   },
-   {
-      category: "general",
-      name: "parent",
-   },
-   {
-      category: "behavior",
-      name: "showWrappedPage",
-   },
-   {
-      category: "type-specific",
-      name: "sizeFalloff",
-   },
-   {
-      category: "type-specific",
-      name: "spacingMode",
-   },
-   {
-      category: "behavior",
-      name: "swipe",
-   },
-   {
-      category: "behavior",
-      name: "swipeMultiplier",
-   },
-   {
-      category: "behavior",
-      name: "swipeResistance",
-   },
-   {
-      category: "behavior",
-      name: "swipeThreshold",
-   },
-   {
-      category: "behavior",
-      name: "transitionFunction",
-   },
-   {
-      category: "behavior",
-      name: "throttle",
-   },
-   {
-      category: "behavior",
-      name: "throttleButtons",
-   },
-   {
-      category: "behavior",
-      name: "throttleKeys",
-   },
-   {
-      category: "behavior",
-      name: "throttleMatchTransition",
-   },
-   {
-      category: "behavior",
-      name: "throttleNavigation",
-   },
-   {
-      category: "behavior",
-      name: "throttleSwipe",
-   },
-   {
-      category: "behavior",
-      name: "throttleTimeout",
-   },
-   {
-      category: "behavior",
-      name: "transition",
-   },
-   {
-      category: "general",
-      name: "type",
-   },
-   {
-      category: "general",
-      name: "visualPreset",
-   },
+let settings = [
+   {name: "autoscroll", category: "Behavioral"},
+   {name: "autoscrollDirection", category: "Behavioral"},
+   {name: "autoscrollPauseOnHover", category: "Behavioral"},
+   {name: "autoscrollSpeed", category: "Behavioral"},
+   {name: "autoscrollStartAfter", category: "Behavioral"},
+   {name: "autoscrollTimeout", category: "Behavioral"},
+   {name: "breakpoints", category: "General"},
+   {name: "id", category: "General"},
+   {name: "infinite", category: "Behavioral"},
+   {name: "keys", category: "Behavioral"},
+   {name: "lazyLoad", category: "Behavioral"},
+   {name: "navigation", category: "General"},
+   {name: "navigationBehavior", category: "Behavioral"},
+   {name: "navigationTrim", category: "Behavioral"},
+   {name: "nextHTML", category: "General"},
+   {name: "pageSpacing", category: "General"},
+   {name: "pageSpacingMode", category: "General"},
+   {name: "pageSpacingUnits", category: "General"},
+   {name: "pages", category: "General"},
+   {name: "pagesToShow", category: "General"},
+   {name: "parent", category: "General"},
+   {name: "prevHTML", category: "General"},
+   {name: "scrollBy", category: "Behavioral"},
+   {name: "showWrappedPage", category: "Behavioral"},
+   {name: "swipe", category: "Behavioral"},
+   {name: "swipeMultiplier", category: "Behavioral"},
+   {name: "swipeResistance", category: "Behavioral"},
+   {name: "swipeThreshold", category: "Behavioral"},
+   {name: "throttle", category: "Behavioral"},
+   {name: "throttleButtons", category: "Behavioral"},
+   {name: "throttleKeys", category: "Behavioral"},
+   {name: "throttleMatchTransition", category: "Behavioral"},
+   {name: "throttleNavigation", category: "Behavioral"},
+   {name: "throttleSwipe", category: "Behavioral"},
+   {name: "throttleTimeout", category: "Behavioral"},
+   {name: "transition", category: "Behavioral"},
+   {name: "transitionFunction", category: "Behavioral"},
+   {name: "uiEnabled", category: "General"},
 ];
-let sortMethod = "category"; // or alphabetical
-
-
-
 
 
 
@@ -193,68 +47,41 @@ function generate() {
    });
 }
 
-// // ### General Settings:
+function sort(method) {
+   let table = document.querySelector(".settings");
+   if (method == "c") {
+      let sorted = [[], []];
+      settings.forEach(setting => {
+         switch (setting.category) {
+            case "General":
+               sorted[0].push(setting);
+               break;
+            case "Behavioral":
+               sorted[1].push(setting);
+               break;
+         }
+      });
+      sorted.forEach(category => {
+         let cat = document.createElement("div");
+         cat.innerHTML = `${category[0].category.toUpperCase()}`;
+         table.appendChild(cat);
+         category.forEach(setting => {
+            let newtr = document.createElement("div");
+            newtr.innerHTML = `${setting.name}`;
+            table.appendChild(newtr);
+         });
+      });
+   } else if (method == "a") {
+      let sorted = [];
+      settings.forEach(setting => {
+         sorted.push(setting.name);
+      });
+      sorted.forEach(setting => {
+         let newtr = document.createElement("div");
+         newtr.innerHTML = setting;
+         table.appendChild(newtr);
+      });
+   }      
+}
 
-// // Setting | Type | Description | Default
-// // --------|------|-------------|--------
-// autoGenCSS | Boolean | true
-// id | String | ".myCarousel"
-// mobile | Object | {swipeThreshold: 50}
-// mobileBreakpoint | Integer | 700
-// navigation | Boolean |true
-// pages | Array |  []
-// parent | String | "body"
-// type | String | "normal"
-// visualPreset | Integer |  0
-
-// // ### Pages settings:
-
-// // Setting | Type | Description
-// // --------|------|------------
-// backgroundImage | String | 
-// css | String | 
-// html | String |
-
-// // ### Behavorial Settings:
-
-// // Setting | Type | Description | Default
-// // --------|------|-------------|--------
-// autoScroll | Boolean | false
-// autoScrollDirection | String | "right"
-// autoScrollPauseOnHover | Boolean |  false
-// autoScrollSpeed | Integer |  5000
-// autoScrollStartAfter | Integer |  5000
-// autoScrollTimeout | Integer | 15000
-// infinite | Boolean |  true
-// keys | Boolean | true
-// navigationBehavior | String |  "nearest"
-// swipe | Boolean |  true
-// swipeMultiplier | Number |  1
-// swipeResistance | Number |  0.95
-// swipeThreshold | Integer |  300
-// showWrappedPage | Boolean |  false
-// transition | Integer | 300
-// transitionFunction | String |  "ease"
-// throttle | Boolean |true
-// throttleButtons | Boolean | true
-// throttleKeys | Boolean |  true
-// throttleMatchTransition | Boolean | true
-// throttleNavigation | Boolean |  true
-// throttleSwipe | Boolean |  true
-// throttleTimeout | Integer |  300
-
-
-// // ### Type-Specific Settings:
-
-// // Setting | Type | Description | Default | Applies to Type(s)
-// // --------|------|-------------|---------|-------------------
-// direction | String |"both" | "stack"
-// enlargeCenter | Integer | 100 | "normal"
-// offsetIn | Integer |  20 | "fade"
-// offsetOut | Integer |  -20 | "fade"
-// offsetUnits | String |  "px" | "fade"
-// sizeFalloff | Integer |  0 | "normal"
-// spacingMode | String | "fill" | "normal", "fade"
-// pageSpacing | Integer |  0 | "normal", "fade"
-// pageSpacingUnits | String |  "px" | "normal", "fade"
-// pagesToShow | Integer | 1 | "normal", "fade"
+sort("a");
