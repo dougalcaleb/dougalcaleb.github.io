aa39ce7c1707f2bea9e931f66371bf75fe1aec9b
Caleb <dougalcaleb@gmail.com>
2021-02-24 23:20:20 -0700

    several pages

---
+++ roundabout/pages/javascript/autoscrollSpeed-settings.js
@@ -0,0 +1,25 @@
+new Roundabout({
+   parent: ".roundabout-example",
+   autoscroll: true,
+   autoscrollSpeed: 4000,
+   pages: [
+      {
+         backgroundImage: "../images/numbers/0.png"
+      },
+      {
+         backgroundImage: "../images/numbers/1.png"
+      },
+      {
+         backgroundImage: "../images/numbers/2.png"
+      },
+      {
+         backgroundImage: "../images/numbers/3.png"
+      },
+      {
+         backgroundImage: "../images/numbers/4.png"
+      },
+      {
+         backgroundImage: "../images/numbers/5.png"
+      }
+   ]
+});
