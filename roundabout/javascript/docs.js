bd7e0905140cec0229d908bffd8fba7170304b23
Caleb <dougalcaleb@gmail.com>
2021-02-24 23:19:36 -0700

    updates to final design

--- roundabout/javascript/docs.js
+++ roundabout/javascript/docs.js
@@ -10,6 +10,7 @@ let settings = [
    {name: "infinite", category: "Behavioral"},
    {name: "keys", category: "Behavioral"},
    {name: "lazyLoad", category: "Behavioral"},
+   {name: "listenForResize", category: "Behavioral"},
    {name: "navigation", category: "General"},
    {name: "navigationBehavior", category: "Behavioral"},
    {name: "navigationTrim", category: "Behavioral"},
@@ -40,6 +41,43 @@ let settings = [
 ];
 
 let sortOrder = "c";
+let sameTab = true;
+
+let msgSrc = window.location.hostname == "127.0.0.1" ? "http://127.0.0.1:5501/roundabout/helper.html" : "https://dougalcaleb.github.io/roundabout/helper.html";
+
+function sendMessage(set = null) {
+   document.getElementById("ls-helper").contentWindow.postMessage(set, msgSrc);
+   // console.log("Message sent to helper");   
+}
+
+
+window.addEventListener("message", (event) => {
+   // console.log("Recieved response", event.data);
+   sortOrder = event.data.sortOrder;
+   sameTab = JSON.parse(event.data.sameTab);
+   if (sortOrder == "c") {
+      document.querySelector(".sort-order").innerHTML = "Sort: by category";
+   } else {
+      document.querySelector(".sort-order").innerHTML = "Sort: alphabetical";
+   }
+   if (sameTab) {
+      document.querySelector(".tab-behavior").innerHTML = "Links: same tab";
+   } else {
+      document.querySelector(".tab-behavior").innerHTML = "Links: new tab";
+   }
+   // console.log(sortOrder);
+   // console.log("Sorting from message response");
+   document.querySelector(".settings").innerHTML = "";
+   sort(sortOrder);
+});
+
+(function setup() {
+   let i = document.getElementById("ls-helper");
+   i.addEventListener("load", function () {
+      sendMessage();
+   });
+   i.src = '/roundabout/helper.html';
+})();
 
 document.querySelector(".sort-order").addEventListener("click", () => {
    if (sortOrder == "c") {
@@ -49,11 +87,36 @@ document.querySelector(".sort-order").addEventListener("click", () => {
       document.querySelector(".sort-order").innerHTML = "Sort: by category";
       sortOrder = "c";
    }
+   try {
+      sendMessage({ sortOrder: sortOrder });
+   } catch (e) {
+      console.error(e);
+      console.warn("Setting could not be saved");
+   }
+   document.querySelector(".settings").innerHTML = "";
+   sort(sortOrder);
+});
+
+document.querySelector(".tab-behavior").addEventListener("click", () => {
+   if (sameTab) {
+      document.querySelector(".tab-behavior").innerHTML = "Links: new tab";
+      sameTab = false;
+   } else {
+      document.querySelector(".tab-behavior").innerHTML = "Links: same tab";
+      sameTab = true;
+   }
+   try {
+      sendMessage({ sameTab: JSON.stringify(sameTab) });
+   } catch (e) {
+      console.error(e);
+      console.warn("Setting could not be saved");
+   }
    document.querySelector(".settings").innerHTML = "";
    sort(sortOrder);
 });
 
 function sort(method) {
+   // console.log(`Sorting with methos ${method}`);
    let table = document.querySelector(".settings");
    if (method == "c") {
       let sorted = [[], []];
@@ -71,11 +134,13 @@ function sort(method) {
          let cat = document.createElement("div");
          cat.innerHTML = `${category[0].category.toUpperCase()}`;
          table.appendChild(cat);
-         cat.classList.add("category-title")
+         cat.classList.add("category-title");
          category.forEach(setting => {
             let newtr = document.createElement("a");
-            newtr.setAttribute("href", `pages/${setting.name}.html`);
-            newtr.setAttribute("target", "_blank");
+            newtr.setAttribute("href", `/roundabout/pages/${setting.name}.html`);
+            if (!sameTab) {
+               newtr.setAttribute("target", "_blank");
+            }
             newtr.innerHTML = `<div>${setting.name}</div>`;
             table.appendChild(newtr);
          });
@@ -87,8 +152,10 @@ function sort(method) {
       });
       sorted.forEach(setting => {
          let newtr = document.createElement("a");
-         newtr.setAttribute("href", `pages/${setting}.html`);
-         newtr.setAttribute("target", "_blank");
+         newtr.setAttribute("href", `/roundabout/pages/${setting}.html`);
+         if (!sameTab) {
+            newtr.setAttribute("target", "_blank");
+         }
          newtr.innerHTML = `<div>${setting}</div>`;
          table.appendChild(newtr);
       });
