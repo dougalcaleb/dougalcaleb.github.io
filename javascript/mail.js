6ea16b98c4aab41f75b999d9b33af5415fbd4ce4
Caleb <dougalcaleb@gmail.com>
2021-03-21 22:57:59 -0600

    improved form

--- javascript/mail.js
+++ javascript/mail.js
@@ -3,21 +3,30 @@ let form = document.getElementById("mail-form");
 async function handleSubmit(event) {
 	event.preventDefault();
 	let status = document.getElementById("form-status");
-	let data = new FormData(event.target);
-	fetch(event.target.action, {
-		method: form.method,
-		body: data,
-		headers: {
-			Accept: "application/json",
-		},
-	})
-		.then((response) => {
-			status.innerHTML = "Thanks for your submission!";
-			form.reset();
-		})
-		.catch((error) => {
-			status.innerHTML = "Oops! There was a problem submitting your form";
-		});
+   let data = new FormData(event.target);
+   if (
+      document.querySelector(".reply-to").value != "" &&
+      document.querySelector(".mail-subject").value != "" &&
+      document.querySelector(".mail-message").value != ""
+   ) {
+      fetch(event.target.action, {
+         method: form.method,
+         body: data,
+         headers: {
+            Accept: "application/json",
+         },
+      })
+         .then((response) => {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset();
+         })
+         .catch((error) => {
+            status.innerHTML = "Oops! There was a problem submitting your form";
+         });
+   } else {
+      status.innerHTML = "Please fill out all fields";
+   }
+	
 }
 
 form.addEventListener("submit", handleSubmit);
