2fead7ca54d4c8db9417dfbe25bab3bfb46878f2
Caleb <dougalcaleb@gmail.com>
2021-03-21 23:01:55 -0600

    cooldown

--- javascript/mail.js
+++ javascript/mail.js
@@ -1,4 +1,9 @@
 let form = document.getElementById("mail-form");
+let cooldown = 8.64e+7;
+
+if (!localStorage.getItem("m-c")) {
+   localStorage.setItem("m-c", 0);
+}
 
 async function handleSubmit(event) {
 	event.preventDefault();
@@ -9,24 +14,28 @@ async function handleSubmit(event) {
       document.querySelector(".mail-subject").value != "" &&
       document.querySelector(".mail-message").value != ""
    ) {
-      fetch(event.target.action, {
-         method: form.method,
-         body: data,
-         headers: {
-            Accept: "application/json",
-         },
-      })
-         .then((response) => {
-            status.innerHTML = "Thanks for your submission!";
-            form.reset();
+      if (Date.now() - localStorage.getItem("m-c") > cooldown) {
+         localStorage.setItem("m-c", Date.now());
+         fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+               Accept: "application/json",
+            },
          })
-         .catch((error) => {
-            status.innerHTML = "Oops! There was a problem submitting your form";
-         });
+            .then((response) => {
+               status.innerHTML = "Thanks for your submission!";
+               form.reset();
+            })
+            .catch((error) => {
+               status.innerHTML = "Oops! There was a problem submitting your form";
+            });
+      } else {
+         status.innerHTML = "You cannot send more than 1 email per day";
+      }
    } else {
       status.innerHTML = "Please fill out all fields";
    }
-	
 }
 
 form.addEventListener("submit", handleSubmit);
