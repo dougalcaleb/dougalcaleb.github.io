7f17f6d22e03d3cdb72ae31d934fef73e82ec9c1
Caleb Dougal <35942679+dougalcaleb@users.noreply.github.com>
2023-10-31 14:02:14 -0600

    Algorithm for moving vertices towards color boundaries in an image (#1)
    * Polygen: Fixed some issues with ThreadManager storing references to threads
    
    * Polygen: algorithm for moving vertices to detected edges
    
    * Polygen: Added logic to reuse computed edges instead of recomputing unnecessarily
    
    * Polygen: Move to Color Boundary is functional

--- portfolio/polygen/javascript/imageToolsWorker.js
+++ portfolio/polygen/javascript/imageToolsWorker.js
@@ -1,8 +1,6 @@
 // TODO:
 /**
- * The basic edge detection kernels work, sort of, but need more aggressive smoothing.
- * Try the Sobel operators and see if that works well enough, but that will likely also be affected by noise a lot.
- * Try to find some way to fine-tune that threshold, because it's kinda just magic numbers right now. Slider maybe? Probably a UI slider.
+ * Proportional neighbor movement isn't implemented
  */
 
 // Bind to thread manager for cleanliness
@@ -10,104 +8,151 @@ self.onmessage = (data) => { ThreadManager.recieve(data); };
 
 // Main function
 function calculateNearestColorLine() {
-	let tStart, tEnd;
+	let tStart, tEnd, adjVerts, edge_result;
 
 	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: 0,
-			new: true,
-			operation: "Grayscale Conversion",
-			begin: true,
-			startTime: Date.now()
-		}
-	})
-
-	tStart = Date.now();
-	const grayscale = Image.convertToGrayscale(Image.imageData);
-	tEnd = Date.now();
-
-	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: tEnd - tStart,
-			new: false,
-			operation: "Grayscale Conversion"
-		}
-	})
-
-	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: 0,
-			new: true,
-			operation: "Image Smoothing",
-			begin: true,
-			startTime: Date.now()
-		}
-	})
-
-	tStart = Date.now();
-	const smoothed = Image.smoothImage(grayscale);
-	tEnd = Date.now();
-
-	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: tEnd - tStart,
-			new: false,
-			operation: "Image Smoothing"
-		}
-	})
-
-	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: 0,
-			new: true,
-			operation: "Edge Detection",
-			begin: true,
-			startTime: Date.now()
-		}
-	})
-
-	tStart = Date.now();
-	const edge_sobel = Image.edgeDetectSobel(smoothed);
-	tEnd = Date.now();
-	Image.draw(edge_sobel);
-
-	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: tEnd - tStart,
-			new: false,
-			operation: "Edge Detection"
-		}
-	})
-
-	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: 0,
-			new: true,
-			operation: "Move Vertices",
-			begin: true,
-			startTime: Date.now()
-		}
-	})
-
-	tStart = Date.now();
-	Image.moveVerticesToLines(edge_sobel);
-	tEnd = Date.now();
-
-	ThreadManager.post({
-		type: "progress",
-		data: {
-			elapsedTime: tEnd - tStart,
-			new: false,
-			operation: "Move Vertices"
+		_threadStatus: {
+			running: true
 		}
 	});
+
+	// If this has been run before (the user is selecting new vertices), there's no need to recompute the edge data
+	if (Image.preComputedEdges) {
+		console.log("FOUND PRECOMPUTED DATA")
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: 0,
+				new: true,
+				operation: "Using existing image data",
+				begin: true,
+				startTime: Date.now()
+			}
+		})
+
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: 0,
+				new: true,
+				operation: "Move Vertices",
+				begin: true,
+				startTime: Date.now()
+			}
+		})
+
+		tStart = Date.now();
+		adjVerts = Image.moveVerticesToLines(Image.preComputedEdges);
+		tEnd = Date.now();
+
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: tEnd - tStart,
+				new: false,
+				operation: "Move Vertices"
+			}
+		});
+	} else { // ==== FIRST - TIME IMAGE DATA GENERATION ====
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: 0,
+				new: true,
+				operation: "Grayscale Conversion",
+				begin: true,
+				startTime: Date.now()
+			}
+		})
+	
+		tStart = Date.now();
+		const grayscale = Image.convertToGrayscale(Image.imageData);
+		tEnd = Date.now();
+	
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: tEnd - tStart,
+				new: false,
+				operation: "Grayscale Conversion"
+			}
+		})
+	
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: 0,
+				new: true,
+				operation: "Image Smoothing",
+				begin: true,
+				startTime: Date.now()
+			}
+		})
+	
+		tStart = Date.now();
+		const smoothed = Image.smoothImage(grayscale);
+		tEnd = Date.now();
+	
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: tEnd - tStart,
+				new: false,
+				operation: "Image Smoothing"
+			}
+		})
+	
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: 0,
+				new: true,
+				operation: "Edge Detection",
+				begin: true,
+				startTime: Date.now()
+			}
+		})
+	
+		tStart = Date.now();
+		edge_result = Image.edgeDetectSobel(smoothed);
+		tEnd = Date.now();
+		Image.draw(edge_result);
+	
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: tEnd - tStart,
+				new: false,
+				operation: "Edge Detection"
+			}
+		})
+	
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: 0,
+				new: true,
+				operation: "Move Vertices",
+				begin: true,
+				startTime: Date.now()
+			}
+		})
+	
+		tStart = Date.now();
+		adjVerts = Image.moveVerticesToLines(edge_result);
+		tEnd = Date.now();
+	
+		ThreadManager.post({
+			type: "progress",
+			data: {
+				elapsedTime: tEnd - tStart,
+				new: false,
+				operation: "Move Vertices"
+			}
+		});
+	}
+
+	// FINAL
 
 	ThreadManager.post({
 		type: "progress",
@@ -115,6 +160,18 @@ function calculateNearestColorLine() {
 			complete: true
 		}
 	})
+
+	ThreadManager.post({
+		_threadStatus: {
+			running: false
+		}
+	});
+
+	ThreadManager.post({
+		type: "result",
+		data: adjVerts,
+		computed: edge_result || Image.preComputedEdges,
+	});
 }
 
 // Contains methods and properties to apply effects to the image
@@ -122,9 +179,12 @@ class Image {
 	// generals
 	static imageData;
 	static selectedVertices;
+	static vertices;
 	static vertexRadius;
 	static height;
 	static width;
+
+	static preComputedEdges;
 
 	// kernels
 	static gaussian = [1, 2, 1, 2, 4, 2, 1, 2, 1];
@@ -236,6 +296,7 @@ class Image {
 		return adjustedPixel;
 	}
 
+	// applies a kernel to a single pixel and returns the value. Supports any size kernel. Significantly slower than the Fast mode.
 	static applyKernelDynamic(index, imageData, kernel) {
 		const kSize = Math.sqrt(kernel.length);
 		const halfKSize = Math.floor(kSize / 2);
@@ -259,6 +320,7 @@ class Image {
 		return adjustedPixel;
 	}
 
+	// given an imagedata array, use the Sobel kernel to edge detect the image
 	static edgeDetectSobel(imageData, edgeThreshold = 100) {
 		let gradientVectors = new Array(imageData.length);
 		Image.KernelSafeIterate((index) => {
@@ -290,12 +352,14 @@ class Image {
 		let smoothedData = new Uint8ClampedArray(imageData);
 		let adjGauss;
 
+		// trim the gaussian kernel's decimals to (hopefully) help with execution time
 		if (useLargeGaussian) {
 			adjGauss = Image.gaussian_large_normalized.map((val) => {
 				return Number(val.toFixed(largeGaussianDec));
 			});
 		}
 
+		// apply the smoothing kernel to the image
 		const kernel = useLargeGaussian ? adjGauss : Image.gaussian_normalized;
 		Image.KernelSafeIterate((index) => {
 			let smoothedPixel = Image.applyKernel(index, imageData, kernel);
@@ -307,6 +371,7 @@ class Image {
 		return smoothedData;
 	}
 
+	// given an image data array, use a simple kernel formula to detect edges (not in use)
 	static edgeDetectSimple(imageData) {
 		const kernel = Image.edge_2_normalized;
 		let edgeData = new Uint8ClampedArray(imageData);
@@ -326,7 +391,6 @@ class Image {
 		for (let a = (Image.width * 4 * halfKSize); a < (Image.width * (Image.height - (1 * halfKSize))) * 4; a += 4) { // skip top & bottom
 			if (a % (Image.width * 4) == 0 || (a + 4) % (Image.width * 4) == 0) { // skip sides
 				continue;
-
 			}
 			callback(a);
 		}
@@ -336,7 +400,8 @@ class Image {
 	static convertToGrayscale(canvasImageData) {
 		let grayscaleData = new Uint8ClampedArray(canvasImageData.data);
 		for (let i = 0; i < grayscaleData.length; i += 4) {
-			const avg = 0.299 * grayscaleData[i] + 0.587 * grayscaleData[i + 1] + 0.114 * grayscaleData[i + 2];
+			// proprietary grayscale algorithm, enjoy the magic numbers
+			const avg = 0.299 * grayscaleData[i] + 0.587 * grayscaleData[i + 1] + 0.114 * grayscaleData[i + 2]; 
 			grayscaleData[i] = avg; // red
 			grayscaleData[i + 1] = avg; // green
 			grayscaleData[i + 2] = avg; // blue
@@ -344,20 +409,61 @@ class Image {
 		return grayscaleData;
 	}
 
+	// given an array of vertices to alter and the binary edge-detected image data, moves vertices to the closest pixel identified as an edge
 	static moveVerticesToLines(imageData) {
-		/** This function will:
-		 * 1. Iterate over all vertices
-		 * 2. For each vertex, find if there are positive pixels within its radius
-		 * 3. Calculate the total distance to move to that pixel
-		 * 4. Move the vertex to that pixel
-		 * 5. Move neighbors proportionally
-		 */
-		
-		// for (const vertex of Image.selectedVertices) {
-		// 	// check in a square, validate with radius
-		// 	for (let idx = -Image.vertexRadius; idx <)
-		// }
+		let adjustedVertices = {};
 
+		// Iterates over each vertex. Checks a square around the vertex (2*radius) and finds increasingly closer positive pixels
+		for (const [id, vertex] of Object.entries(Image.selectedVertices)) {
+			let closestPixel = null;
+			let closestDistance = Infinity;
+			let origin = [~~vertex.coord[0], ~~vertex.coord[1]];
+
+			for (let x = origin[0] - Image.vertexRadius; x < origin[0] + Image.vertexRadius; x++) {
+				for (let y = origin[1] - Image.vertexRadius; y < origin[1] + Image.vertexRadius; y++) {
+					const distance = Math.sqrt((origin[0] - x) ** 2 + (origin[1] - y) ** 2);
+
+					if (distance < closestDistance) {
+						const index = (y * Image.width + x) * 4;
+						const r = imageData[index];
+						const g = imageData[index + 1];
+						const b = imageData[index + 2];
+						const a = imageData[index + 3];
+						if (r === 255 && g === 255 && b === 255 && a === 255) {
+							closestPixel = { x, y };
+							closestDistance = distance;
+						}
+					}
+				}
+			}
+
+			if (closestPixel) {
+				// console.log(`discovered: ${closestPixel.x},${closestPixel.y} is closest to ${origin[0]},${origin[1]} after checking ${checkIts} pixels with bounds ${~~vertex.coord[0] - Image.vertexRadius},${~~vertex.coord[1] - Image.vertexRadius} :: ${~~vertex.coord[0] + Image.vertexRadius},${~~vertex.coord[1] + Image.vertexRadius}`);
+				adjustedVertices[id] = {
+					id: vertex.id,
+					coord: [closestPixel.x, closestPixel.y]
+				};
+			}
+
+			// * Not sure if any of this is useful or not, but still need to add logic to move neighbors proportionally
+			// if (closestPixel) {
+			// 	const dx = closestPixel.x - vertex.x;
+			// 	const dy = closestPixel.y - vertex.y;
+			// 	vertex.x = closestPixel.x;
+			// 	vertex.y = closestPixel.y;
+
+			// 	// move neighbors proportionally
+			// 	for (const neighbor of vertex.neighbors) {
+			// 		const ndx = neighbor.x - vertex.x;
+			// 		const ndy = neighbor.y - vertex.y;
+			// 		const distance = Math.sqrt(ndx * ndx + ndy * ndy);
+			// 		neighbor.x = vertex.x + ndx / dx * distance;
+			// 		neighbor.y = vertex.y + ndy / dy * distance;
+			// 	}
+			// }
+		}
+
+		return adjustedVertices;
 	}
 }
 
@@ -371,10 +477,15 @@ class ThreadManager {
 
 	static recieve(data) {
 		Image.imageData = data.data.canvas;
-		Image.selectedVertices = structuredClone(data.data.selectedVerts);
+		Image.selectedVertices = data.data.selectedVerts;
+		Image.vertices = data.data.allVerts;
 		Image.height = data.data.canvas.height;
 		Image.width = data.data.canvas.width;
-		Image.vertexRadius = data.data.radius;
+		Image.vertexRadius = ~~data.data.radius;
+		Image.preComputedEdges = data.data.preCompute;
+
+		console.log("WORK RECIEVED");
+		console.log(data.data.preCompute);
 
 		calculateNearestColorLine();
 	}
