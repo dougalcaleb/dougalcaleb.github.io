47449d2df2a14fff41db6bc1b6042d9373ef4d69
Caleb <dougalcaleb@gmail.com>
2024-04-01 23:06:56 -0600

    Update todo

--- portfolio/polygen/javascript/script.js
+++ portfolio/polygen/javascript/script.js
@@ -4,8 +4,13 @@ import UI from "./controllers/ui.js";
 /* 
 TODO:
 
-- Sticky section in editor panel (panel side, layers)
-- Preview resolution scale slider
+- Outline weight instead of outline opacity
+- Reseed randomness button
+- Ensure everything works properly with multiple layers, types, etc
+- Start on reimplementation of the editor functions
+
+! Known bug:
+- Create new layer after having already created an image layer, the new layer will be of type "linear" but the settings will be for an image layer
 
 */
 
