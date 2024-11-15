f7f21bc61db079de799a3d5f3c60f4002dc5f693
Caleb <dougalcaleb@gmail.com>
2024-11-14 20:12:53 -0700

    Remove all old files. Start fresh.

--- hosting/BYU/wrtg150-advocacy/javascript/store.js
+++
@@ -1,4 +0,0 @@
-export class Store {
-	static pages = [];
-	static main = null;
-}
--- portfolio/scss-resume/javascript/script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", function() {
-    var top = window.scrollY;
-    document.querySelector(".resume").style.top = (100+(top/2))+"px";
-});
