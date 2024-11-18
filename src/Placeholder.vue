fc7cd9a9ea8b09ed88723798497ba12467f12783
Caleb <dougalcaleb@gmail.com>
2024-11-17 18:01:48 -0700

    Add close placeholder

--- src/Placeholder.vue
+++ src/Placeholder.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="flex justify-center absolute top-0 w-full z-50">
-		<div class="w-78 h-max bg-gray-4 text-white font-subtitle font-bold mt-8 shadow-lg shadow-gray-0 rounded-md py-4 px-12">
+		<div class="w-78 h-max bg-gray-4 text-white font-subtitle font-bold mt-8 shadow-lg shadow-gray-0 rounded-md py-4 px-16 relative">
+			<Icon icon="fa/xmark" :size="4" class="text-red-2 absolute right-4 cursor-pointer" @click="destroy"/>
 			<p class="text-center">This site is currently under construction!</p>
 			<p class="text-center">Please check back soon for the full thing.</p>
 		</div>
@@ -8,9 +9,11 @@
 </template>
 
 <script>
+import Icon from './components/common/Icon.vue';
+
 export default {
 	components: {
-		
+		Icon,
 	},
 	props: {
 		
@@ -24,7 +27,9 @@ export default {
 		
 	},
 	methods: {
-		
+		destroy() {
+			this.$el.parentNode.removeChild(this.$el);
+		}
 	}
 }
 </script>
