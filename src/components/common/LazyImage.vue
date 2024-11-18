c9aab6554eda00ca7481f6f41b0cb17f7e3ea5c8
Caleb <dougalcaleb@gmail.com>
2024-11-17 17:04:23 -0700

    Add LazyImage component

---
+++ src/components/common/LazyImage.vue
@@ -0,0 +1,48 @@
+<template>
+	<img :alt="alt" ref="img" >
+</template>
+
+<script>
+export default {
+	components: {
+		
+	},
+	props: {
+		src: {
+			type: String,
+			required: true,
+		},
+		alt: {
+			type: String,
+			default: "",
+		},
+		onLoad: {
+			type: Function,
+			default: () => {},
+		},
+	},
+	data() {
+		return {
+			imgObj: null,
+		}
+	},
+	computed: {
+		
+	},
+	methods: {
+		imgLoaded() {
+			this.$refs['img'].src = this.src;
+			this.onLoad();
+		}
+	},
+	created() {
+		this.imgObj = new Image();
+		this.imgObj.onload = this.imgLoaded;
+		this.imgObj.src = this.src;
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
